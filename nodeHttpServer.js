292e52e5b989b9c3e9ad06df398331d5a3787566
ylehilds <lehi_alcantara@byu.edu>
2023-03-23 13:52:35 -0600

    renamed a variable

--- nodeHttpServer.js
+++ nodeHttpServer.js
@@ -4,7 +4,7 @@ const hostname = '127.0.0.1'
 const port = 8000
 
 // Create HTTP server
-const nodeHttpServer = http.createServer((req, res) => {
+const server = http.createServer((req, res) => {
   // Set the response HTTP header with HTTP and Content type
   res.writeHead(200, {'Content-Type': 'text/plain'})
   // Send the response body 'Welcome the start learning node and express'
@@ -12,6 +12,6 @@ const nodeHttpServer = http.createServer((req, res) => {
 })
 
 // Prints a log once the server starts listening
-nodeHttpServer.listen(port, hostname, () => {
+server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`)
 })
